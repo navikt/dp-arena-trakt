7c3fc78da3000582877cec95691f23a84c84c4f2
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2021-11-24 09:12:55 +0100

    Dropp reject key og test jsonlogging for kibana
    ref: navikt/dagpenger#989
    
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- src/main/kotlin/no/nav/dagpenger/arena/trakt/hendelser/BeregningsleddoppdateringService.kt
+++ src/main/kotlin/no/nav/dagpenger/arena/trakt/hendelser/BeregningsleddoppdateringService.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.arena.trakt.hendelser
 
 import mu.KotlinLogging
+import net.logstash.logback.argument.StructuredArguments.keyValue
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
@@ -8,22 +9,21 @@ import no.nav.helse.rapids_rivers.River
 
 val log = KotlinLogging.logger {}
 
-const val MAX_ANTALL_MELDINGER_LEST = 100
+const val MAX_ANTALL_MELDINGER_LEST = 10000
 
 class BeregningsleddoppdateringService(rapidsConnection: RapidsConnection) : River.PacketListener {
-    var meldingerLest = 0;
+    var meldingerLest = 0
 
     init {
         River(rapidsConnection).apply {
             validate {
-                it.rejectKey("@id")
             }
         }
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        if(meldingerLest < MAX_ANTALL_MELDINGER_LEST) {
-            log.info { packet }
+        if (meldingerLest < MAX_ANTALL_MELDINGER_LEST) {
+            log.info("log message {} {}", keyValue("ting", "pakke motatt"), keyValue("innhold", packet))
             meldingerLest++
         }
     }
