55422b1ae3c6672100492cbcaa38c1a6d98a0ce3
André Roaldseth <andre.roaldseth@nav.no>
2021-12-07 09:38:14 +0100

    Start innlesning Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no> Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- src/main/kotlin/no/nav/dagpenger/arena/trakt/ApplicationBuilder.kt
+++ src/main/kotlin/no/nav/dagpenger/arena/trakt/ApplicationBuilder.kt
@@ -3,7 +3,6 @@ package no.nav.dagpenger.arena.trakt
 import mu.KotlinLogging
 import no.nav.dagpenger.arena.trakt.db.DataRepository
 import no.nav.dagpenger.arena.trakt.db.HendelseRepository
-import no.nav.dagpenger.arena.trakt.db.PostgresDataSourceBuilder.clean
 import no.nav.dagpenger.arena.trakt.db.PostgresDataSourceBuilder.runMigration
 import no.nav.dagpenger.arena.trakt.tjenester.BeregningsleddService
 import no.nav.dagpenger.arena.trakt.tjenester.DataMottakService
@@ -18,10 +17,10 @@ val log = KotlinLogging.logger {}
 internal class ApplicationBuilder(config: Map<String, String>) : StatusListener {
     private val rapidsConnection = RapidApplication.Builder(
         RapidApplication.RapidApplicationConfig.fromEnv(config)
-    ).build { _, kafkaRapid ->
+    ).build() /*{ _, kafkaRapid ->
         kafkaRapid.seekToBeginning()
         clean()
-    }
+    }*/
 
     init {
         rapidsConnection.register(this)
