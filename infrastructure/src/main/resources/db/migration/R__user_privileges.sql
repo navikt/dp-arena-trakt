289292c5274454745c6e2e15cf15041e0354bb96
André Roaldseth <andre.roaldseth@nav.no>
2022-01-28 09:56:30 +0100

    Gi job brukeren skrivetilgang til arena_data

--- infrastructure/src/main/resources/db/migration/R__user_privileges.sql
+++ infrastructure/src/main/resources/db/migration/R__user_privileges.sql
@@ -1,11 +1,22 @@
-
-DO $$
+DO
+$$
     BEGIN
         IF EXISTS
-            ( SELECT 1 from pg_roles where rolname='cloudsqliamuser')
+            (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
         THEN
             GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
-        END IF ;
+        END IF;
     END
-$$ ;
+$$;
 
+-- Gi naisjob-brukeren skrivetilgang
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 FROM pg_user WHERE usename = 'job')
+        THEN
+            GRANT INSERT ON arena_data TO job;
+        END IF;
+    END
+$$;
