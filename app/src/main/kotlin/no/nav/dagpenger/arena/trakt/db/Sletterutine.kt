46be0ea5d30e4e27c4dea24c7e7ea503d64533b5
Marius Eriksen <marius.eriksen@nav.no>
2022-02-08 10:50:10 +0100

    Bruk kotlin sin wrapper for Timer istedenfor Java sin

--- app/src/main/kotlin/no/nav/dagpenger/arena/trakt/db/Sletterutine.kt
+++ app/src/main/kotlin/no/nav/dagpenger/arena/trakt/db/Sletterutine.kt
@@ -1,8 +1,7 @@
 package no.nav.dagpenger.arena.trakt.db
 
 import mu.KotlinLogging
-import java.util.Timer
-import java.util.TimerTask
+import kotlin.concurrent.fixedRateTimer
 
 private val logg = KotlinLogging.logger {}
 
@@ -11,21 +10,18 @@ internal class Sletterutine internal constructor(
     private val msFørSletterutineBegynner: Long = 1000L,
     private val msMellomSlettinger: Long = 10000L
 ) {
+    private val batchStørrelse = 100000
 
-    private class SletteTask(val dataRepository: DataRepository) : TimerTask() {
-        override fun run() {
-            val batchStørrelse = 100000
+    internal fun start() {
+        fixedRateTimer(
+            "Sletterutine",
+            daemon = true, // JVMen avsluttes når det kun er daemon tråden som kjører
+            initialDelay = msFørSletterutineBegynner,
+            period = msMellomSlettinger
+        ) {
             logg.info { "Sletterutine starter" }
             val raderSlettet = dataRepository.batchSlettDataSomIkkeOmhandlerDagpenger(batchStørrelse)
             logg.info { "Rader slettet: ${raderSlettet.sum()}" }
         }
-    }
-
-    internal fun start() {
-        Timer().schedule(
-            SletteTask(dataRepository),
-            msFørSletterutineBegynner,
-            msMellomSlettinger
-        )
     }
 }
