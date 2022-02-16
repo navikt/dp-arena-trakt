02edcdf7eb125f7a470c4b555acdfb91eac001af
Marius Eriksen <marius.eriksen@nav.no>
2022-02-16 14:04:11 +0100

    Fix feilende test for deploy
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>

--- app/src/test/kotlin/no/nav/dagpenger/arena/trakt/db/DataRepositoryTest.kt
+++ app/src/test/kotlin/no/nav/dagpenger/arena/trakt/db/DataRepositoryTest.kt
@@ -35,7 +35,7 @@ internal class DataRepositoryTest {
             dataRepository.lagre(vedtaksfaktaJSON(dagpengeVedtakId), tabell = "SIAMO.VEDTAKFAKTA")
             dataRepository.lagre(beregningsleddJSON(dagpengeVedtakId), tabell = "SIAMO.BEREGNINGSLEDD")
 
-            assertEquals(3, antallOppdateringerForVedtak(dagpengeVedtakId))
+            assertEquals(0, antallOppdateringerForVedtak(dagpengeVedtakId))
         }
     }
 
