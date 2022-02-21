1345d6016e67ed0ba43cd8911d46d83a84a95667
André Roaldseth <andre.roaldseth@nav.no>
2022-02-21 14:19:03 +0100

    Tilbake til timer og latency-måling.

--- app/src/main/kotlin/no/nav/dagpenger/arena/trakt/db/Sletterutine.kt
+++ app/src/main/kotlin/no/nav/dagpenger/arena/trakt/db/Sletterutine.kt
@@ -4,14 +4,14 @@ import io.prometheus.client.Histogram
 import mu.KotlinLogging
 import mu.withLoggingContext
 import java.util.UUID
-import kotlin.concurrent.fixedRateTimer
+import kotlin.concurrent.timer
 
 private val logg = KotlinLogging.logger {}
 
 internal class Sletterutine internal constructor(
     private val dataRepository: DataRepository,
     private val msFørSletterutineBegynner: Long = 1000L,
-    private val msMellomSlettinger: Long = 10000L,
+    private val msMellomSlettinger: Long = 100L,
     private val batchStørrelse: Int = 1000
 ) {
     companion object {
@@ -22,7 +22,7 @@ internal class Sletterutine internal constructor(
             .register()
     }
 
-    internal fun start() = fixedRateTimer(
+    internal fun start() = timer(
         "Sletterutine",
         daemon = true, // JVMen avsluttes når det kun er sletterutine tråden som kjører
         initialDelay = msFørSletterutineBegynner,
@@ -31,10 +31,10 @@ internal class Sletterutine internal constructor(
         val runId = UUID.randomUUID()
         withLoggingContext("runId" to runId.toString()) {
             logg.info { "Sletterutine starter" }
-            // slettejobbLatency.time {
-            val raderSlettet = dataRepository.batchSlettDataSomIkkeOmhandlerDagpenger(batchStørrelse)
-            logg.info { "Rader slettet: ${raderSlettet.sum()}" }
-            // }
+            slettejobbLatency.time {
+                val raderSlettet = dataRepository.batchSlettDataSomIkkeOmhandlerDagpenger(batchStørrelse)
+                logg.info { "Rader slettet: ${raderSlettet.sum()}" }
+            }
         }
     }
 }
