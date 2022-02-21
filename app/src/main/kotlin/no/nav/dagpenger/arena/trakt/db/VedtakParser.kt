a671d09f54822fc158f8bbb89438eae41356d5c8
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-02-21 11:00:48 +0100

    wip: vedtaksparser
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>
    Co-authored-by: Maria Cabrol <maria.cabrol@nav.no>

--- app/src/main/kotlin/no/nav/dagpenger/arena/trakt/db/VedtakParser.kt
+++ app/src/main/kotlin/no/nav/dagpenger/arena/trakt/db/VedtakParser.kt
@@ -1,11 +1,21 @@
 package no.nav.dagpenger.arena.trakt.db
 
 internal class VedtakParser {
-    fun parse(vedtaksData: List<String>): DagpengeVedtak {
-        return UfullstendigDagpengeVedtak
+    //Forstår json og skal produserer vedtak
+
+    fun parse(vedtaksData: List<String>): DagpengeVedtak = lagVedtak(vedtaksData)
+
+    private fun lagVedtak(vedtaksData: List<String>): DagpengeVedtak {
+        /* vedtaksData.forEach {
+             val json = ObjectMapper().readTree(it.reader())
+             when(json["tabell"].asText()){
+             }
+         }*/
+        return UfullstendigDagpengeVedtak;
     }
+
 
     internal interface DagpengeVedtak
     internal class FullstendigDagpengevedtak : DagpengeVedtak
-    internal object UfullstendigDagpengeVedtak : DagpengeVedtak
+    internal object UfullstendigDagpengeVedtak : DagpengeVedtak //nullobject
 }
