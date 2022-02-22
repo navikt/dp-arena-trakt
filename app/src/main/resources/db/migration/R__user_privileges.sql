c6e30cf290513aa3282053939be45436b50e62d8
André Roaldseth <andre.roaldseth@nav.no>
2022-02-22 15:32:18 +0100

    Fjern ubrukte database-schemas

--- app/src/main/resources/db/migration/R__user_privileges.sql
+++ app/src/main/resources/db/migration/R__user_privileges.sql
@@ -8,17 +8,3 @@ $$
         END IF;
     END
 $$;
-
--- Gi naisjob-brukeren skrivetilgang
-DO
-$$
-    BEGIN
-        IF EXISTS
-            (SELECT 1 FROM pg_user WHERE usename = 'job')
-        THEN
-            GRANT INSERT ON arena_data TO job;
-            GRANT USAGE, SELECT ON SEQUENCE arena_data_id_seq TO job;
-            GRANT USAGE, SELECT ON SEQUENCE arena_data_sletterekkefolge_seq TO job;
-        END IF;
-    END
-$$;
