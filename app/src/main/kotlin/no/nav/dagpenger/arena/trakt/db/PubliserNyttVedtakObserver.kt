26200dcbb6acad6afce5f140021ff26b4ad3cea7
André Roaldseth <andre.roaldseth@nav.no>
2022-02-22 22:34:25 +0100

    Logg når vi publiserer nytt vedtak

--- app/src/main/kotlin/no/nav/dagpenger/arena/trakt/db/PubliserNyttVedtakObserver.kt
+++ app/src/main/kotlin/no/nav/dagpenger/arena/trakt/db/PubliserNyttVedtakObserver.kt
@@ -1,13 +1,23 @@
 package no.nav.dagpenger.arena.trakt.db
 
+import mu.KotlinLogging
+import mu.withLoggingContext
 import no.nav.dagpenger.arena.trakt.db.HendelseRepository.Companion.fraVedtak
 import no.nav.dagpenger.arena.trakt.db.VedtakRepository.VedtakObserver
 import no.nav.dagpenger.arena.trakt.tjenester.VedtakService.Vedtak
+
+private val logger = KotlinLogging.logger {}
 
 internal class PubliserNyttVedtakObserver(
     private val hendelseRepository: HendelseRepository
 ) : VedtakObserver {
     override fun nyttDagpengeVedtak(vedtak: Vedtak) {
-        hendelseRepository.publiser(fraVedtak(vedtak))
+        withLoggingContext(
+            "sakId" to vedtak.sakId.toString(),
+            "vedtakId" to vedtak.vedtakId.toString(),
+        ) {
+            logger.info { "Publiserer nytt dagpengevedtak" }
+            hendelseRepository.publiser(fraVedtak(vedtak))
+        }
     }
 }
