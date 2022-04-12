bf8570b9eeb2ee46bad51b67ff7bba478ff1a35d
Giao The Cung <giaothe@gmail.com>
2022-04-12 10:59:00 +0200

    Upgrade kotlin version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -51,7 +51,7 @@ object Dagpenger {
 object Database {
     const val Postgres = "org.postgresql:postgresql:42.2.22"
     const val Kotlinquery = "com.github.seratch:kotliquery:1.6.1"
-    const val Flyway = "org.flywaydb:flyway-core:8.5.0"
+    const val Flyway = "org.flywaydb:flyway-core:7.10.0"
     const val HikariCP = "com.zaxxer:HikariCP:4.0.3"
     const val VaultJdbc = "no.nav:vault-jdbc:1.3.7"
 }
@@ -117,16 +117,16 @@ object Konfig {
 }
 
 object Kotlin {
-    const val version = "1.6.0"
+    const val version = "1.6.20"
     const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
 
     object Coroutines {
-        const val version = "1.4.2"
+        const val version = "1.6.1"
         fun module(name: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$name:$version"
     }
 
     object Logging {
-        const val version = "2.0.11"
+        const val version = "2.1.21"
         const val kotlinLogging = "io.github.microutils:kotlin-logging:$version"
     }
 }
@@ -216,7 +216,7 @@ object Prometheus {
     }
 }
 
-const val RapidAndRivers = "com.github.navikt:rapids-and-rivers:2022.02.08-20.42.8e24a34a2ecd"
+const val RapidAndRivers = "com.github.navikt:rapids-and-rivers:2022.03.25-09.17.0628678a7192"
 
 object Slf4j {
     const val version = "1.7.25"
